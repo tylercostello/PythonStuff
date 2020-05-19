c38af45eab71d22e5bb38b10ebfb1d1adb466e55
tyco333 <34726618+tyco333@users.noreply.github.com>
2020-05-19 11:55:51 -0700

    I lied the first time but now I actually know linear algebra anad calc 3

---
+++ scratchnn2.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+def sig(x):
+    return 1/(1+np.exp(-x))
+
+def sigprime(x):
+    return sig(x)*(1-sig(x))
+
+hiddenneurons=2
+outputneurons=3
+input=np.random.rand(1,1)
+l1w=np.random.rand(1,hiddenneurons)
+l2w=np.random.rand(hiddenneurons,outputneurons)
+layer1=sig(np.dot(input,l1w))
+
+def feedforward(x):
+    input[0,0]=x
+    layer1=sig(np.dot(input,l1w))
+    return sig(np.dot(sig(np.dot(input,l1w)),l2w))
+print(feedforward(1))
+#print(l2w)
+
+def dw2():
+    return np.dot(sigprime(np.dot(layer1,l2w)).T,layer1).T
+def dw1():
+    #return np.multiply(input,np.multiply(sigprime(np.dot(layer1,l2w)),np.dot(sigprime(np.dot(input,l1w)),l2w)))
+    return np.multiply(input,np.multiply(np.dot(sigprime(np.dot(layer1,l2w)),l2w.T),sigprime(np.dot(input,l1w))))
+print(dw1())
+print(dw2())

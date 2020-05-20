5f3a4f173dff49b579fd684bc411fb7028cc2d64
tyco333 <34726618+tyco333@users.noreply.github.com>
2020-05-19 18:47:14 -0700

    made it more customizable

--- scratchnn2.py
+++ scratchnn2.py
@@ -5,23 +5,32 @@ def sig(x):
 
 def sigprime(x):
     return sig(x)*(1-sig(x))
-
-hiddenneurons=2
-outputneurons=3
-input=np.random.rand(1,1)
-l1w=np.random.rand(1,hiddenneurons)
+myinput=0.8
+inputneurons=5
+hiddenneurons=5
+outputneurons=5
+input=np.random.rand(1,inputneurons)
+l1w=np.random.rand(inputneurons,hiddenneurons)
 l2w=np.random.rand(hiddenneurons,outputneurons)
 layer1=sig(np.dot(input,l1w))
-
+print("input")
+print(myinput)
+print("weights1")
+print(l1w)
+print("weights2")
+print(l2w)
 def feedforward(x):
     input[0,0]=x
     layer1=sig(np.dot(input,l1w))
     return sig(np.dot(sig(np.dot(input,l1w)),l2w))
-print(feedforward(1))
+print("output")
+print(feedforward(myinput))
 
 def dw2():
     return np.dot(sigprime(np.dot(layer1,l2w)).T,layer1).T
 def dw1():
     return np.multiply(input,np.multiply(np.dot(sigprime(np.dot(layer1,l2w)),l2w.T),sigprime(np.dot(input,l1w))))
+print("d1")
 print(dw1())
+print("d2")
 print(dw2())

1fb372608a23261152e7d945e7543fa117d86cc4
tyco333 <34726618+tyco333@users.noreply.github.com>
2020-05-25 12:57:07 -0700

    Softmax derivative kinda works

--- ML/nnscratchclass.py
+++ ML/nnscratchclass.py
@@ -1,17 +1,43 @@
 import numpy as np
+import warnings
+
+warnings.simplefilter(action = "ignore", category = RuntimeWarning)
 class NN:
     def sig(self,x):
         return 1/(1+np.exp(-x))
     def sigprime(self,x):
         return self.sig(x)*(1-self.sig(x))
+
     def soft(self,x):
         return np.exp(x)/np.sum(np.exp(x))
+
     def softprime(self,x):
-        #s = x.reshape(-1,1)
+        s = x.reshape(-1,1)
         #print(x.shape)
         #print((np.diagflat(s) - np.dot(s, s.T)).shape)
-        #return (np.diagflat(s) - np.dot(s, s.T))
-        return self.soft(x)*(1-self.soft(x))
+        jacobian = (np.diagflat(s) - np.dot(s, s.T))
+        #print(jacobian.shape)
+        rowone=jacobian[0,:]
+
+        rowone=np.reshape(rowone,(1,-1))
+
+        sum=jacobian.sum(axis=1)
+        sum=np.reshape(sum,(1,-1))
+        diag=np.diag(jacobian,k=0)
+        diag=np.reshape(diag,(1,-1))
+        #print(rowone.shape)
+        #print(rowone)
+        #column=np.zeros((len(jacobian),1))
+        #print(len(jacobian))
+        #for counter in range(len(jacobian)):
+            #print(jacobian[counter:].sum())
+        #    column[counter]=(jacobian[counter:].sum())/len(jacobian)
+        #return column.T
+        #return rowone
+        #return sum
+        return diag
+
+        #return self.soft(x)*(1-self.soft(x))
     def __init__(self,In,Hn,On):
         self.inputneurons=In
         hiddenneurons=Hn
@@ -38,18 +64,19 @@ class NN:
     def dw1(self):
         layer1=self.sig(np.dot(self.input,self.l1w))
         return np.multiply(np.dot(np.dot(self.input.T,self.softprime(np.dot(layer1,self.l2w))),self.l2w.T).T,self.sigprime(np.dot(self.input,self.l1w)).T).T
-testInput=np.array([[0.3]])
+testInput=np.array([[0.2]])
 print("input")
 print(testInput)
-newNN=NN(1,5,9)
+newNN=NN(1,2,3)
 print("output")
 #print(newNN.feedforward(np.array([[-0.5,-0.5]])))
 print(newNN.feedforward(testInput))
 for x in range(10000):
-    newNN.addw1(newNN.dw1())
-
-    w2adder=newNN.dw2()
+    newNN.addw1(0.01*newNN.dw1())
+    #newNN.addw2(newNN.dw2())
+    w2adder=0.01*newNN.dw2()
     w2adder[:,0]*=-1
+    w2adder[:,1]*=-1
     newNN.addw2(w2adder)
     #newNN.feedforward(np.array([[-0.5,-0.5]]))
     newNN.feedforward(testInput)

223440661182d49244159d7ed5259b2a4bd6eca1
tyco333 <34726618+tyco333@users.noreply.github.com>
2020-06-26 17:45:11 -0700

    Added Set

--- REHS/Spellchecker_and_NER/ner.py
+++ REHS/Spellchecker_and_NER/ner.py
@@ -1,3 +1,5 @@
+import time
+start_time = time.time()
 def getList(file):
     with open(file, encoding="utf8") as f:
         list = f.read().split(" ")
@@ -6,14 +8,18 @@ def lowerCaseList(upperList):
     lowerList=[x.lower().rstrip("\n") for x in upperList]
     return lowerList
 
-namesList=[]
 
-namesList+=getList("FullNamesList.txt")
 
-email=getList("sample.txt")
+namesList=set(getList("FullNamesList.txt"))
+
+email=getList("sample2.txt")
 email=lowerCaseList(email)
+
+
+
 newEmail = [x for x in email if x not in namesList]
 print("Original: ")
 print(email)
 print("With NER: ")
 print(newEmail)
+print("--- %s seconds ---" % (time.time() - start_time))

4f772311943bbadbb5edd7f657de7a1bf6f57578
tyco333 <34726618+tyco333@users.noreply.github.com>
2020-07-06 00:51:20 -0700

    variable name change

--- Kalman/sinekalman.py
+++ Kalman/sinekalman.py
@@ -7,27 +7,27 @@ np.random.seed(1)
 t = np.arange(0.0, 2.0, 0.01)
 v=np.sin(2 * np.pi * t)
 s = np.sin(2 * np.pi * t)
-print(np.var(s))
+#print(np.var(s))
 s=s+np.random.normal(0, 0.2, s.shape)
-print(np.var(s))
+#print(np.var(s))
 
 
 pointsList=[]
 r=0.5057002776436168
 q=0.05
 pc=0
-g=0
+k=0
 p=1
 xp=0
 zp=0
 xe=0
 for counter in range(200):
     pc=p+q
-    g=pc/(pc+r)
-    p=(1-g)*pc
+    k=pc/(pc+r)
+    p=(1-k)*pc
     xp=xe
     zp=xp
-    xe=g*(s[counter]-zp)+xp
+    xe=k*(s[counter]-zp)+xp
     pointsList.append(xe)
 
 fig, ax = plt.subplots()

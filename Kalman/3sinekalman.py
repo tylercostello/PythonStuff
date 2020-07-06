4d2c6840e7e6abe22e96231d79a41b44a9a22bc2
tyco333 <34726618+tyco333@users.noreply.github.com>
2020-07-06 13:07:11 -0700

    Added triple sensors and commented

--- Kalman/3sinekalman.py
+++ Kalman/3sinekalman.py
@@ -3,21 +3,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-#time
+
 t = np.arange(0.0, 2.0, 0.01)
-#true function
 v=np.sin(2 * np.pi * t)
-#sensor input
 s = np.sin(2 * np.pi * t)
+s2 = np.sin(2 * np.pi * t)
+s3 = np.sin(2 * np.pi * t)
+#print(np.var(s))
 np.random.seed(1)
 s=s+np.random.normal(0, 0.2, s.shape)
+np.random.seed(2)
+s2=s2+np.random.normal(0, 0.2, s2.shape)
+np.random.seed(3)
+s3=s3+np.random.normal(0, 0.2, s3.shape)
 #print(np.var(s))
 
 
-
 pointsList=[]
-r=0.5057002776436168 # sensor covariance, done by np.var(s)
-q=0.05
+r=0.5057002776436168
+q=0.01
 pc=0
 k=0
 p=1
@@ -31,23 +35,30 @@ for counter in range(200):
     k=pc/(pc+r)
     #update covariance
     p=(1-k)*pc
-    #update state
+    #update state 3 times
     xp=xe
     zp=xp
     xe=k*(s[counter]-zp)+xp
+    xp=xe
+    zp=xp
+    xe=k*(s2[counter]-zp)+xp
+    xp=xe
+    zp=xp
+    xe=k*(s3[counter]-zp)+xp
     pointsList.append(xe)
-
-#calculates average distance between true function and kalman filter
 sav=0
+
 for newCounter in range(200):
     sav=sav+(pointsList[newCounter]-v[newCounter])
 sav=sav/200
 print(sav)
-
-#plotting
 fig, ax = plt.subplots()
+
 #ax.plot(t, s)
+#ax.plot(t,s2)
 ax.plot(t, v)
 ax.plot(t,pointsList,color='red')
+
 ax.grid()
+
 plt.show()

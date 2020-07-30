f9d44332c45907f6068f5e1ee8baea285aed2827
tyco333 <34726618+tyco333@users.noreply.github.com>
2020-07-30 02:08:41 -0700

    Cleaned up code

--- Kalman/advancedarc.py
+++ Kalman/advancedarc.py
@@ -6,8 +6,8 @@ import matplotlib.pyplot as plt
 t = np.arange(0.0, 14.0, 0.01)
 
 #from what I understand this is the wheel's forward velocity, so it is radius * angular velocity
-sl=2
-sr=2.00001
+sl=50
+sr=25
 x=0
 y=0
 theta=np.pi/2
@@ -22,38 +22,10 @@ m1=np.array([
             [theta]
             ],
             dtype='float')
-w=(sr-sl)/b
-r=(b/2)*(sl+sr)/(sr-sl)
-icc=np.array([
-            [m1[0]-r*np.sin(m1[2])],
-            [m1[1]+r*np.cos(m1[2])]
-            ],
-            dtype='float')
-m2=np.array([
-            [np.cos(w*dt), -np.sin(w*dt),0],
-            [np.sin(w*dt),np.cos(w*dt),0],
-            [0,0,1]
-            ],
-            dtype='float')
-#print(m2.shape)
-m3=np.array([
-            [m1[0]-icc[0]],
-            [m1[1]-icc[1]],
-            [m1[2]]
-            ],
-            dtype='float')
 
-m3.shape=(3,1)
-#print(m3.shape)
-m4=np.array([
-            [icc[0]],
-            [icc[1]],
-            [w*dt]
-            ],
-            dtype='float')
 for counter in range(1399):
 
-    m1=np.add(np.matmul(m2,m3),m4)
+
     w=(sr-sl)/b
     r=(b/2)*(sl+sr)/(sr-sl)
     icc=np.array([
@@ -84,7 +56,7 @@ for counter in range(1399):
                 ],
                 dtype='float')
 
-
+    m1=np.add(np.matmul(m2,m3),m4)
     xList.append(m1[0])
     yList.append(m1[1])
 plt.xlim(-10,10)
